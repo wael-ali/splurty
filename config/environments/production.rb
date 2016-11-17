ef535605a8b2ae1963cbd9316f27edf0c92dce3c
wael ali <waelali595@gmail.com>
2016-11-18 00:56:07 +0100

    Adjust productionn.rb so it compiles assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+  #Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.asset.compile = false
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -23,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
