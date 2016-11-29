3a18c2adc35d51b7bf69205f4a5892385707d043
wael ali <waelali595@gmail.com>
2016-11-29 17:50:00 +0100

    Dealing with errors but not good enough

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,9 @@
 class Quote < ApplicationRecord
+
+validates :saying, presence: true, length: {maximum: 140, minimum: 3}
+
+  validates :auther, presence: true, length: {maximum: 50, minimum:3}
+
+
+  
 end
