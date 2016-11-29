90c9046a3438436368a89365bce4afef74bf2e9a
wael ali <waelali595@gmail.com>
2016-11-29 16:44:44 +0100

    entering data in the data base thruogh our form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,4 +9,16 @@ class QuotesController < ApplicationController
     @quote = Quote.new
     
   end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+    
+  end
+
+  private
+  def quote_params
+    params.require(:quote).permit(:saying, :auther)
+    
+  end
 end
