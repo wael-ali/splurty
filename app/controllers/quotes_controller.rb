e2e908cf998ebc0954c0e28c6d172852b7860599
wael ali <waelali595@gmail.com>
2016-12-02 22:15:36 +0100

    error input message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,7 +11,10 @@ class QuotesController < ApplicationController
     @quote = Quote.create(quote_params)
 
     if @quote.invalid?
-      flash[:error] = '<strong>Could not  save</strong>the data you entered is invalid.'
+      flash[:error] = '<strong>Could not  save ...</strong>
+      <br> the data you entered is invalid !!!!
+      <br> For the Quote : between 3 - 140 charechters
+      <br> For the Auther : between 3 - 50 charechters '
       
     end
 
