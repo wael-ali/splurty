e5bb40d7b2db1ec51d9d8ab3580233b283b59542
wael-ali <waelali595@gmail.com>
2019-04-14 21:09:38 +0200

    finished crud for Code model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'quotes#index'
 
   resources :quotes
+  resources :codes
   get 'about', to: 'quotes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
