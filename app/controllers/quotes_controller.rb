9a62c00c5469dfee8c623261b3bed2047d45dcb5
wael-ali <waelali595@gmail.com>
2019-04-14 22:03:21 +0200

    new home page, fix if no codes in index to show

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,7 +15,7 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
-  def about
+  def home
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   resources :words
 
-  root 'quotes#index'
+  root 'quotes#home'
 
   resources :quotes
   resources :codes
-  get 'about', to: 'quotes#about'
+  #get 'about', to: 'quotes#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
