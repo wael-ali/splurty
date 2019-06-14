03233eb365acb2f274daa85bd86c48433cf00eb6
wael-ali <waelali595@gmail.com>
2019-06-14 23:49:09 +0200

    validate word, equivalent, change them to lowercase before saving

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,13 @@
 class Word < ApplicationRecord
+    validates :name, presence: true
+    validates :equivalent, presence: true
+    validates :sourc_language, presence: true
+    validates :target_language, presence: true
+
+    before_save :downcase_fieldss
+
+  def downcase_fields
+    self.name.downcase
+    self.equivalent.downcase
+  end
 end
